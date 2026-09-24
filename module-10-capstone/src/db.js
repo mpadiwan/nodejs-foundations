0453d78427e0c57654cdb72c611f074fd5c6fc8f
Margaux Padiwan <mpadiwan@stratpoint.com>
2026-09-25 05:36:03 +0800

    chore: setup health check route

---
+++ module-10-capstone/src/db.js
@@ -0,0 +1,9 @@
+require("dotenv/config");
+
+const { PrismaClient } = require("../generated/prisma/client.mts");
+const { PrismaBetterSqlite3 } = require("@prisma/adapter-better-sqlite3");
+
+const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+module.exports = prisma;
